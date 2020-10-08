4eda231f7552969037a59918b8bb4188d6f9ffc8
Triol <35421298+Triol@users.noreply.github.com>
2020-10-08 16:33:33 +0800

    Update Main.java

--- src/main/java/entrance/Main.java
+++ src/main/java/entrance/Main.java
@@ -3,6 +3,7 @@ package entrance;
 import job.search.forgery.SearchForgeryJob;
 import job.search.image.SearchImageJob;
 import job.search.part.SearchPartImageJob;
+import job.upload.UploadImages;
 import utils.ImageUtils;
 
 import java.io.File;
@@ -15,41 +16,51 @@ public class Main {
         args[3] taskName
      */
     public static void main(String[] args) {
-        if(args.length != 4) {
-            System.out.println("Usages: java -jar <program> <SearchType> <inputFile(file)> <outputPath(dir)> <taskName>");
+        if(args.length != 4 && (args.length == 3 && !"UploadImage".equals(args[0]))) {
+            System.out.println("Usages1: java -jar <program> <SearchType> <inputFile(file)> <outputPath(dir)> <taskName>");
             System.out.println("<SearchType>: SearchPart SearchImage SearchForgery");
             System.out.println("<SearchType>: 0          1           2");
             System.out.println("<inputFile> : inputFile: must be a image file");
             System.out.println("<outputPath>: outputPath: must be a directory");
+            System.out.println("Usages2: java -jar <program> UploadImage <Path(dir)>");
         }else {
-            File inputFile = new File(args[1]);
-            if(inputFile == null || !inputFile.isFile()) {
-                System.err.println("inputFile error, please check the path");
-            }
-            File outputDir = new File(args[2]);
-            if(outputDir == null || !outputDir.isDirectory()) {
-                System.err.println("outputPath error, must be a directory, please check");
+            if("UploadImage".equals(args[0])) {
+                try  {
+                    UploadImages.uploadImage(args[1]);
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }else {
+                File inputFile = new File(args[1]);
+                if(inputFile == null || !inputFile.isFile()) {
+                    System.err.println("inputFile error, please check the path");
+                }
+                File outputDir = new File(args[2]);
+                if(outputDir == null || !outputDir.isDirectory()) {
+                    System.err.println("outputPath error, must be a directory, please check");
+                }
+
+                if(args[0].equals("0") || args[0].equals("SearchPart")) {
+                    try {
+                        SearchPartImageJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }else if(args[0].equals("1") || args[0].equals("SearchImage")) {
+                    try {
+                        SearchImageJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }else if(args[0].equals("2") || args[0].equals("SearchForgery")) {
+                    try {
+                        SearchForgeryJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
+                    }catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
             }
 
-            if(args[0].equals("0") || args[0].equals("SearchPart")) {
-                try {
-                    SearchPartImageJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }else if(args[0].equals("1") || args[0].equals("SearchImage")) {
-                try {
-                    SearchImageJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }else if(args[0].equals("2") || args[0].equals("SearchForgery")) {
-                try {
-                    SearchForgeryJob.doJob(args[3], ImageUtils.getImage(inputFile), args[2]);
-                }catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 }
